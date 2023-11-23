1230dd8fdd05f575205c27d1193f33745e098872
telekrex <telekrex@gmail.com>
2023-11-23 11:17:43 -0500

    Update new.py

--- Source/new.py
+++ Source/new.py
@@ -7,14 +7,13 @@ width = window.winfo_screenwidth() * .6
 height = window.winfo_screenheight() * .6
 size = f'{round(width)}x{round(height)}'
 window.geometry(size)
-
-
-current_file = "C:/Users/nitro/Documents/GitHub/image-viewer/Test Images/crab.png"
-current_image = ImageTk.PhotoImage(Image.open(current_file))
-
-
 display = Label(window, image=None)
 display.pack(anchor='center', expand=True)
+
+
+# Test
+current_file = "C:/Users/nitro/Documents/GitHub/image-viewer/Test Images/crab.png"
+current_image = ImageTk.PhotoImage(Image.open(current_file))
 
 
 def update():
@@ -22,12 +21,26 @@ def update():
     display.configure(image=current_image)
 
 
+def open_folder(event):
+    print('opening!')
 
 
+def nxt(event):
+    print('next!')
 
+
+def prv(event):
+    print('previous!')
+
+
+def refresh(event):
+    print('refreshing!')
+    update()
+
+
+window.bind('<o>', open_folder)
+window.bind('<Right>', nxt)
+window.bind('<Left>', prv)
+window.bind('<r>', refresh)
 update()
-
-
-
-
 window.mainloop()
